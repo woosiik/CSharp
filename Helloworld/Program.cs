4860feffd424953a8333617f611e1fc3ed652958
woosiik <65073092+woosiik@users.noreply.github.com>
2021-03-08 09:30:20 +0900

    Add files via upload

---
+++ Helloworld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello CSharp world!!!");
+			//Console.ReadLine();
+			Console.WriteLine("2017648084 정우석");
+		}
+	}
+}
