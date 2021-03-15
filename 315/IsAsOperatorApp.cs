6484c983071283844fcbfef63465306bdfc30135
woosiik <65073092+woosiik@users.noreply.github.com>
2021-03-15 10:39:30 +0900

    Add files via upload

---
+++ 315/IsAsOperatorApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class IsAsOperatorApp
+{
+    static void IsOperator(object obj)
+    {
+        Console.WriteLine(obj + " is int : " + (obj is int));
+        Console.WriteLine(obj + " is string : " + (obj is string));
+    }
+    static void AsOperator(object obj)
+    {
+        Console.WriteLine(obj + " as string == null : " + (obj as string == null));
+    }
+    public static void Main()
+    {
+        IsOperator(10);
+        IsOperator("ABC");
+        AsOperator(10);
+        AsOperator("ABC");
+    }
+}
---
+++ 315/PropertyApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class PropertyClass
+{
+    private int privateValue;
+    public int PrivateValue
+    {
+        get { return privateValue; } // get-accessor
+        set { privateValue = value; } // set-accessor
+    }
+    public void PrintValue()
+    {
+        Console.WriteLine("Hidden Value = " + privateValue);
+    }
+}
+class PropertyApp
+{
+    public static void Main()
+    {
+        int n;
+        PropertyClass obj = new PropertyClass();
+        obj.PrivateValue = 100; // invoke set-accessor
+        obj.PrintValue();
+        n = obj.PrivateValue; // invoke get-accessor
+        Console.WriteLine(" Value = " + n);
+    }
+}
